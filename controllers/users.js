4839e5524ced0e8a9f739359da316c7655d91851
Jiya Singh <jiyasingh2401@gmail.com>
2025-10-17 18:18:30 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup = async(req, res) => {
         if (err) {
             return next(err);
         }
-        req.flash("success", "Welcome to Wanderlust");
+        req.flash("success", "Welcome to ExploreX");
          res.redirect("/listings");
     });
 
@@ -30,7 +30,7 @@ module.exports.renderLoginForm =  (req, res) => {
 };
 
 module.exports.login = async (req, res) => {
-    req.flash("success", "Welcome to Wanderlust!");
+    req.flash("success", "Welcome to ExploreX!");
     let redirectUrl = res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
 };
